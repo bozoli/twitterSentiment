77ec64d9c7cf564b4f1e8a0c9ef374c83645a62f
bozoli <38265083+bozoli@users.noreply.github.com>
2019-03-11 17:04:21 -0300

    Create twitterKeys.py

---
+++ Server Side Scripts/twitterKeys.py
@@ -0,0 +1,6 @@
+
+# Twitter Secrets and Keys
+consumer_key = 'Your Consumer Key Here'
+consumer_secret = 'Your Consumer Secret Here'
+access_token_key = 'Your Access Token Key Here'
+access_token_secret = 'Your Access Token Secret Here'
